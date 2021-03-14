aae232720f00d033e88a1a6e4ea2dd5e5fbe6236
Johann Taylor <j.taylor1343@gmail.com>
2021-03-13 23:46:49 -0500

    Created homepage view

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
